5da0f7ec5461558028d63fde243cba4e36eddeef
Vilasi <VilasiCoding@gmail.com>
2023-05-23 22:13:00 -0400

    Add Cost of Living Calc initial stylings

---
+++ public/js/ColCalc.js
@@ -0,0 +1,32 @@
+const FORM = document.querySelector('form');
+
+FORM.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userInputs = makeRequest(e.target.children);
+  //   Example Output: [ "jezebel ", "ca", "clark-pa", "45000" ]
+  window.location.href = `https://www.bestplaces.net/cost-of-living/${userInputs[0]}-${userInputs[1]}/${userInputs[2]}/${userInputs[3]}`;
+});
+
+function makeRequest(children) {
+  const valueArray = [];
+  for (let child of children) {
+    for (let el of child.children) {
+      if (el.classList.contains('userInput')) {
+        valueArray.push(el.value);
+      }
+    }
+  }
+
+  valueArray[0] = lowerCase(valueArray[0]);
+  valueArray[0] = replaceWhiteSpace(valueArray[0]);
+
+  return valueArray;
+}
+
+function lowerCase(str) {
+  return str.toLowerCase();
+}
+
+function replaceWhiteSpace(str) {
+  return str.replace(/ /g, '-');
+}
