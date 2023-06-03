fba0657c5150b50d0166534a05d1980f6da30d81
Vilasi <VilasiCoding@gmail.com>
2023-06-03 03:10:03 -0400

    Began constructing dialog

--- public/js/home/modal.js
+++ public/js/home/modal.js
@@ -6,3 +6,18 @@ const dialog = document.querySelector('#modal-test');
 modalButton.addEventListener('click', (e) => {
   dialog.showModal();
 });
+
+//* This allows the user to close the modal if they click outside of it
+// dialog.addEventListener('click', (e) => {
+//   const dialogDimensions = dialog.getBoundingClientRect();
+//   console.log(dialogDimensions);
+//   console.log(e);
+//   if (
+//     e.clientX < dialogDimensions.left ||
+//     e.clientX > dialogDimensions.right ||
+//     e.clientY < dialogDimensions.top ||
+//     e.clientY > dialogDimensions.bottom
+//   ) {
+//     dialog.close();
+//   }
+// });
