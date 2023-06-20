fc98e20435774f5291cbf5d28995f651b9526f03
Vilasi <VilasiCoding@gmail.com>
2023-06-20 04:17:57 -0400

    Edit footer comment. Add events schema in /models

--- models/events.js
+++ models/events.js
@@ -5,19 +5,47 @@ const objectIdType = Schema.Types.ObjectId;
 const eventSchema = new Schema({
   title: {
     type: String,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   date: {
-    type: String,
+    type: Date,
+    required: true,
   },
   location: {
     type: String,
+    required: true,
   },
-  time: {
-    type: String,
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User model
+    required: true,
   },
+  attendees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+    },
+  ],
+
+  // title: {
+  //   type: String,
+  // },
+  // description: {
+  //   type: String,
+  // },
+  // date: {
+  //   type: String,
+  // },
+  // location: {
+  //   type: String,
+  // },
+  // time: {
+  //   type: String,
+  // },
   // image: {
   //   type: String,
   // },
