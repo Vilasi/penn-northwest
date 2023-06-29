88ed185780a4fb82f9f60023bacb1121a6c3d14d
Vilasi <VilasiCoding@gmail.com>
2023-06-28 23:25:57 -0400

    JavaScript href linkage functionality working in headerLinks.js -> membership.ejs

--- public/js/membership/headerLinks.js
+++ public/js/membership/headerLinks.js
@@ -2,13 +2,25 @@ const headerLinks = document.querySelectorAll('.header-links');
 const membershipButtons = document.querySelector('#membership-buttons');
 console.log(headerLinks);
 
+//* Article functionality
+//? This loop ensures that every article within the membershipButtons section has href behavior/functionality
 for (let article of membershipButtons.children) {
   article.addEventListener('mouseover', (e) => {
     article.style.cursor = 'pointer';
   });
 
+  //* Provides a href intra-page link to the header buttons
   article.addEventListener('click', (e) => {
-    console.log('hi');
-    window.location.href = '#test';
+    const classlist = e.target.classList;
+
+    if (classlist.contains('incentives-link-anchor')) {
+      window.location.href = '#membership-incentives';
+    }
+    if (classlist.contains('signup-link-anchor')) {
+      window.location.href = '#signup';
+    }
+    if (classlist.contains('member-list-link-anchor')) {
+      window.location.href = '#member-list';
+    }
   });
 }
