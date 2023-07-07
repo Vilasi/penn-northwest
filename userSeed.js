e1b1aeca5b4d6f6426b6bdfcbac086c53c623455
Vilasi <VilasiCoding@gmail.com>
2023-07-06 21:00:16 -0400

    In userSeed.js add delete functionality if foundUser is extant

--- userSeed.js
+++ userSeed.js
@@ -1,3 +1,5 @@
+//* This seed file creates a single user account
+
 const mongoose = require('mongoose');
 const express = require('express');
 const User = require('./models/users');
@@ -12,7 +14,15 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/penn-northwest');
 }
 
+//* The following function creates a single user
+//? If the user has already been created, it first deletes that user.
 async function addDevAccount() {
+  const foundUser = await User.findByUsername('vilasi');
+  if (foundUser) {
+    console.log(foundUser);
+    await User.findByIdAndDelete(foundUser._id);
+  }
+
   const user = new User({
     email: 'vilasicoding@gmail.com',
     username: 'vilasi',
