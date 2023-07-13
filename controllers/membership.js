eb03b98fa4aedc7c00dbbaebd07f4690cc6eb21c
Vilasi <VilasiCoding@gmail.com>
2023-07-13 04:15:32 -0400

    Honeypot input field complete. Members list UI started

--- controllers/membership.js
+++ controllers/membership.js
@@ -33,6 +33,11 @@ module.exports.getLevelsBrochure = (req, res, next) => {
 
 //* Membership Application Submission Functionality
 module.exports.handleMembershipForm = async (req, res, next) => {
+  // Honeypot Catch Field
+  if (req.body.honey) {
+    return res.redirect('/pages/error');
+  }
+
   // Validate reCAPTCHA response
   const captchaValidateBoolean = await validateReCaptcha(req);
 
