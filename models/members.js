dda02f5fefdf7aa4c655976aa585e742de7f997b
Vilasi <VilasiCoding@gmail.com>
2023-07-27 01:26:32 -0400

    Filtered members array created without alphabet (a, b, c, etc)

--- models/members.js
+++ models/members.js
@@ -11,24 +11,23 @@ const memberSchema = new Schema({
   },
 });
 
-let childList5 = document.querySelector('#testtesttesttest').children;
-
-let allMembers4 = [...childList5].map((p) => {
-  if (p.children.length > 0) {
-    return {
-      href: `${p.children[0].href}`,
-      name: `${p.children[0].innerText}`,
-    };
-  } else {
-    return {
-      href: ``,
-      name: `${p.innerText}`,
-    };
-  }
+const filteredArray = array.filter((obj) => {
+  return obj.href !== 'undefined';
 });
 
-// for (let p of childList) {
-//     if (p.children.length > 0) {
+//* This is for retrieving the members list from the original website
+// let childList5 = document.querySelector('#testtesttesttest').children;
 
-//     }
-// }
+// let allMembers4 = [...childList5].map((p) => {
+//   if (p.children.length > 0) {
+//     return {
+//       href: `${p.children[0].href}`,
+//       name: `${p.children[0].innerText}`,
+//     };
+//   } else {
+//     return {
+//       href: ``,
+//       name: `${p.innerText}`,
+//     };
+//   }
+// });
