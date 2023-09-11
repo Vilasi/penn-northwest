c1314b5f74deda2cd17c20301ca0ab211f369322
Vilasi <VilasiCoding@gmail.com>
2023-09-11 16:21:01 -0400

    Mkdir admin in views dir. Added resizing script/styles to navbar partial in views dir

--- public/js/navAnimation.js
+++ public/js/navAnimation.js
@@ -13,13 +13,21 @@
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('.navbar');
   const hrElements = document.querySelectorAll('hr.m-0');
+  const elementsToHide = document.querySelectorAll('.scroll-hide');
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 0) {
     navbar.classList.add('scroll');
     hrElements.forEach((hr) => hr.classList.add('scroll'));
+    elementsToHide.forEach((el) => {
+      console.log(el);
+      return el.classList.add('to-hide');
+    });
   } else {
     navbar.classList.remove('scroll');
     hrElements.forEach((hr) => hr.classList.remove('scroll'));
+    elementsToHide.forEach((el) => {
+      return el.classList.remove('to-hide');
+    });
   }
 });
