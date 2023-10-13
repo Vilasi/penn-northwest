3fd39b1c235a1d9c210599226c4cd282f7aa34a7
Vilasi <VilasiCoding@gmail.com>
2023-10-12 23:40:48 -0400

    Created about page storyboard

--- config/cors/index.js
+++ config/cors/index.js
@@ -10,7 +10,6 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
-// app.use(cors());
 
 //* This will function as a middleware - the function call will itself return a function that we can use
 module.exports = cors(corsOptions);
