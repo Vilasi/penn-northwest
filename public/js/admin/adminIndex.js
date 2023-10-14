6337cdee61d92a6e75c05de24a003ab66a23c8b6
Vilasi <VilasiCoding@gmail.com>
2023-10-14 14:33:08 -0400

    Added DOM state management to Admin Panel

--- public/js/admin/adminIndex.js
+++ public/js/admin/adminIndex.js
@@ -1,34 +1,54 @@
 const adminNavButton = document.querySelectorAll('.adminNavButton');
 const dataSections = document.querySelectorAll('.data-sections');
-console.log(adminNavButton);
-console.log(dataSections);
 
-// Admin Panel Button Functionality
+//* Load section state
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('buttonIndex')) {
+    const index = localStorage.getItem('buttonIndex');
+    const btn = document.querySelector(`[data-button-index="${index}"]`);
+
+    hideSections(btn);
+    showSection(btn);
+  }
+});
+
+//* Click Listener for Admin Panel Button Statefulness Functionality -  hide/show sections
 adminNavButton.forEach((btn) => {
-  //This adds/removes the 'active' class on click
   btn.addEventListener('click', (e) => {
-    for (let btn of adminNavButton) {
-      btn.classList.remove('active');
-      btn.removeAttribute('aria-selected', 'true');
-      btn.setAttribute('aria-current', 'false');
+    // Set the state in localStorage
+    localStorage.setItem('buttonIndex', btn.dataset.buttonIndex);
 
-      // This hides every section on any button press . The proper one will be unhidden in the below loops
-      dataSections.forEach((section) => section.classList.add('d-none'));
-    }
-    btn.classList.add('active');
-    btn.setAttribute('aria-current', 'true');
+    // Hide/Show relevant sections
+    hideSections(btn);
+    showSection(btn);
+  });
+});
 
-    // This iterates through the clicked button's classes and matches them with each data sections classes to show
-    // When there is one match (there will only be one match) is removed the d-none class.
-    // This will ensure that only the correct section is shown on the correct button press
-    for (let attribute of btn.classList) {
-      for (let section of dataSections) {
-        for (let identifier of section.classList) {
-          if (identifier === attribute) {
-            section.classList.remove('d-none');
-          }
+//* Begin statefulness function definitions
+function hideSections(button) {
+  for (let btn of adminNavButton) {
+    btn.classList.remove('active');
+    btn.removeAttribute('aria-selected', 'true');
+    btn.setAttribute('aria-current', 'false');
+
+    // This hides every section on any button press . The proper one will be unhidden in the below showSection function
+    dataSections.forEach((section) => section.classList.add('d-none'));
+  }
+  button.classList.add('active');
+  button.setAttribute('aria-current', 'true');
+}
+
+// This iterates through the clicked button's classes and matches them with each data sections classes to show
+// When there is one match (there will only be one match) is removed the d-none class.
+// This will ensure that only the correct section is shown on the correct button press
+function showSection(button) {
+  for (let attribute of button.classList) {
+    for (let section of dataSections) {
+      for (let identifier of section.classList) {
+        if (identifier === attribute) {
+          section.classList.remove('d-none');
         }
       }
     }
-  });
-});
+  }
+}
