447fe9da6593bf90d6be589869708bff6c9091ee
Vilasi <VilasiCoding@gmail.com>
2024-01-09 21:59:25 -0500

    Update instructions for sendUsernameEmail env reminder

--- utils/middleware/sendUsernameEmail.js
+++ utils/middleware/sendUsernameEmail.js
@@ -3,6 +3,8 @@ const sgMail = require('@sendgrid/mail');
 const apiKey = process.env.SENDGRID_EMAIL_API_KEY;
 sgMail.setApiKey(apiKey);
 
+// TODO Change Verified sender to env variable
+
 function sendUsernameEmail(user, url) {
   const message = {
     to: `${user.email}`,
