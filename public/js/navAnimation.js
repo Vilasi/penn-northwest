64c79e346a1bbd485aecb1b4985ce976d3e91a4c
Vilasi <VilasiCoding@gmail.com>
2024-03-07 21:11:43 -0500

    Refactored nav logo to use brand image instead of custom markup

--- public/js/navAnimation.js
+++ public/js/navAnimation.js
@@ -12,18 +12,21 @@
 
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('.navbar');
+  const logoImage = document.querySelector('#logo-image');
   const hrElements = document.querySelectorAll('hr.m-0');
   const elementsToHide = document.querySelectorAll('.scroll-hide');
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 0) {
     navbar.classList.add('scroll');
+    logoImage.classList.add('logo-resize');
     hrElements.forEach((hr) => hr.classList.add('scroll'));
     elementsToHide.forEach((el) => {
       return el.classList.add('to-hide');
     });
   } else {
     navbar.classList.remove('scroll');
+    logoImage.classList.remove('logo-resize');
     hrElements.forEach((hr) => hr.classList.remove('scroll'));
     elementsToHide.forEach((el) => {
       return el.classList.remove('to-hide');
