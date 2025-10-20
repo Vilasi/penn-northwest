b3feade354e88b1760db1ee7ff9ab33722c3e2a7
gacharyzoniea <zgoniea@gmail.com>
2025-10-20 17:50:50 -0400

    email sign up and other additions
    -Footer has been reformatted to look sleeker and overall fill available space better
    -New button enables opening a popup to sign up for newsletter via email
    -Social Icons have more flair and Twitter/X's logo has been updated

--- config/content-security-policy/index.js
+++ config/content-security-policy/index.js
@@ -7,6 +7,9 @@ module.exports.scriptSrcUrls = [
   'https://cdn.jsdelivr.net',
   'https://www.google.com/',
   'https://www.gstatic.com/',
+  'https://static.ctctcdn.com/',
+  'http://static.ctctcdn.com/',
+  
 ];
 module.exports.styleSrcUrls = [
   'https://kit-free.fontawesome.com/',
@@ -17,12 +20,22 @@ module.exports.styleSrcUrls = [
   'https://use.fontawesome.com/',
   'https://cdn.jsdelivr.net/',
   'https://cdnjs.cloudflare.com/',
+  'https://static.ctctcdn.com/',
+  'http://static.ctctcdn.com/',
 ];
 module.exports.connectSrcUrls = [
   'https://api.mapbox.com/',
   'https://a.tiles.mapbox.com/',
   'https://b.tiles.mapbox.com/',
   'https://events.mapbox.com/',
+  'https://static.ctctcdn.com/',
+  'http://static.ctctcdn.com/',
+  'https://listgrowth.ctctcdn.com/',
+  'http://listgrowth.ctctcdn.com/',
+  'https://visitor2.constantcontact.com/',
+  'http://visitor2.constantcontact.com/',
+  'https://www.google.com/',
+  'http://www.google.com/',
 ];
 module.exports.fontSrcUrls = [
   'https://fonts.googleapis.com/',
@@ -34,4 +47,5 @@ module.exports.imgSrcUrls = [
   'https://placehold.co',
   'https://res.cloudinary.com/dypchgtip/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
   'https://images.unsplash.com/',
+  'https://static.ctctcdn.com/',
 ];
